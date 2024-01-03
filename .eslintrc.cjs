e6cb6ac91711ee2e550363cda6e23f761cb47a9c
sihyeon95 <psh@devtools.kr>
2024-01-04 02:26:26 +0900

    Lint: vue component naming rule 변경
    컴포넌트 폴더 내 한개의 뎁스가 더 있는 경우 두 단어 이상의 컴포넌트를 선언할 필요 없도록 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,4 +12,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   }
+  overrides: [
+    {
+      files: ['src/components/*/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0,
+      }
+    }
+  ]
 }
