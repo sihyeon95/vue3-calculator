93bfc983cb4e981dea76385207b75cba35b2cdcc
sihyeon95 <psh@devtools.kr>
2024-01-03 23:11:59 +0900

    Feat: 표준 계산기 마크업

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <main></main>
+  <main class="calculator-app">
+    <standard-calculator></standard-calculator>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import StandardCalculator from './components/StandardCalculator.vue'
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.calculator-app {
+  height: 100vh;
+  padding: 8px 12px;
+}
+</style>
---
+++ src/components/StandardCalculator.vue
@@ -0,0 +1,102 @@
+<template>
+  <section class="calculator-view">
+    <div class="calculator-expression">
+      <p class="expression">{{ expression }}</p>
+      <p class="statement">{{ statement }}</p>
+    </div>
+    <div class="button-wrapper">
+      <button
+        v-for="({ label, type, value }, idx) in buttons"
+        :key="idx"
+        v-html="label"
+        @click="() => pressButton(type, value)"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+interface Button {
+  label: string
+  type: 'number' | 'operator'
+  value?: number
+}
+
+const expression = ref('')
+const statement = ref('0')
+
+const buttons: Button[] = [
+  { label: '%', type: 'operator' },
+  { label: 'CE', type: 'operator' },
+  { label: 'C', type: 'operator' },
+  { label: '&#x232B;', type: 'operator' },
+  { label: '⅟x', type: 'operator' },
+  { label: 'x<sup>2</sup>', type: 'operator' },
+  {
+    label: '<sup>2</sup>&radic;<span style="text-decoration:overline;">&nbsp;x&nbsp;</span>',
+    type: 'operator'
+  },
+  { label: '&divide;', type: 'operator' },
+  { label: '7', type: 'number', value: 7 },
+  { label: '8', type: 'number', value: 8 },
+  { label: '9', type: 'number', value: 9 },
+  { label: '&times;', type: 'operator' },
+  { label: '4', type: 'number', value: 4 },
+  { label: '5', type: 'number', value: 5 },
+  { label: '6', type: 'number', value: 6 },
+  { label: '-', type: 'operator' },
+  { label: '1', type: 'number', value: 1 },
+  { label: '2', type: 'number', value: 2 },
+  { label: '3', type: 'number', value: 3 },
+  { label: '+', type: 'operator' },
+  { label: '&plusmn;', type: 'operator' },
+  { label: '0', type: 'number' },
+  { label: '.', type: 'number' },
+  { label: '=', type: 'operator' }
+]
+
+function pressButton(type: 'number' | 'operator', value?: number) {
+  console.log(type, value)
+}
+</script>
+
+<style lang="scss">
+.calculator-view {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.calculator-expression {
+  min-height: 100px;
+  text-align: end;
+  margin-bottom: 16px;
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  .statement {
+    font-size: 72px;
+    font-weight: bold;
+  }
+}
+
+.button-wrapper {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 8px;
+  flex: 2;
+  button {
+    text-align: center;
+    padding: 18px 20px;
+    border-radius: 8px;
+    border: solid 1px $g3;
+    box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.05);
+    cursor: pointer;
+    font-size: 24px;
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
