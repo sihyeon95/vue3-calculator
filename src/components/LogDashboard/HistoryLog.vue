0575960fed22616132387a39283cd9425c85086b
Park Sihyeon <psh@devtools.kr>
2024-01-04 15:07:30 +0900

    Feat: 기록 영역 트렌지션 적용 및 렌더 순서 변경

--- src/components/LogDashboard/HistoryLog.vue
+++ src/components/LogDashboard/HistoryLog.vue
@@ -1,15 +1,20 @@
 <template lang="html">
   <div class="history-log">
     <p v-if="historys.length === 0">아직 기록이 없음</p>
-    <div v-else v-for="(history, index) in historys" :key="index">
-      <p class="expression text-g4" v-html="expressionFilter(history.expression, `    `) + ` = `" />
-      <p class="statement text-bold">{{ history.result }}</p>
-    </div>
+    <TransitionGroup v-else name="slide">
+      <div class="historys" v-for="(history, index) in historys" :key="index">
+        <p
+          class="expression text-g4"
+          v-html="expressionFilter(history.expression, `    `) + ` = `"
+        />
+        <p class="statement text-bold">{{ history.result }}</p>
+      </div>
+    </TransitionGroup>
   </div>
 </template>
 <script lang="ts" setup>
-import {History} from '@/type/Log'
-import {expressionFilter} from '@/util/StringFilter'
+import { History } from '@/type/Log'
+import { expressionFilter } from '@/util/StringFilter'
 
 interface Props {
   historys: History[]
@@ -20,9 +25,26 @@ defineProps<Props>()
 .history-log {
   text-align: right;
   width: 100%;
+  display: flex;
+  flex-direction: column-reverse;
+
+  .historys:not(:first-child) {
+    margin-bottom: 40px;
+  }
 
   .statement {
     font-size: 24px;
+    margin-top: 10px;
   }
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s ease;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
 }
 </style>
